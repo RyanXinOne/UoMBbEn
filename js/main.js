2491d203c1edff1386580121aad1ba17dc8b35c9
RyanXin <xyz@ryanxin.cn>
2021-02-11 21:41:20 +0800

    Adaptive daylight saving time (GMT/BST). Thanks for Axton.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 const enabledCourses = ["COMP10120", "COMP11120", "COMP11212", "COMP13212", "COMP15212", "COMP16412"];
 const sessionLinks = {
-    "COMP10120 - Mon Live": "https://zoom.us/j/99834452668#success",
-    "COMP10120 - Tue Lab": "https://zoom.us/j/99472897815#success",
-    "COMP10120 - Thu Tutorial": "https://zoom.us/j/93017662193#success",
+    "COMP10120 Team - Mon Live": "https://zoom.us/j/99834452668#success",
+    "COMP10120 Team - Tue Lab": "https://zoom.us/j/99472897815#success",
+    "COMP10120 Team - Thu Tutorial": "https://zoom.us/j/93017662193#success",
     "COMP11120 Math - Tue Example - 328882": "https://zoom.us/j/91297341121#success",
     "COMP11120 Math - Tur Live - 084290": "https://zoom.us/j/99774198906#success",
     "COMP11212 Computation": "https://zoom.us/j/93199465841#success",
@@ -32,13 +32,10 @@ function renderCourses() {
 }
 function calculateTime() {
     // calculate UK time
-    let targetTimeZone = 0;  //UK time zone (GMT)
-    let offset_GMT = new Date().getTimezoneOffset();
-    let nowDate = new Date().getTime();
-    let targetDate = new Date(nowDate + offset_GMT * 60 * 1000 + targetTimeZone * 60 * 60 * 1000);
     let formatNumber = (n) => { return n < 10 ? "0" + n.toString() : n.toString() };
     const weekDay = { 0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday" };
     const months = { 0: "Jan", 1: "Feb", 2: "Mar", 3: "Apr", 4: "May", 5: "Jun", 6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec" };
+    let targetDate = new Date(new Date().toLocaleString('en', { timeZone: 'Europe/London' }));
     let readableTime = formatNumber(targetDate.getHours()) + ":" + formatNumber(targetDate.getMinutes()) + ":" + formatNumber(targetDate.getSeconds()) + " " + weekDay[targetDate.getDay()] + ", " + targetDate.getDate() + " " + months[targetDate.getMonth()];
     return readableTime;
 }
@@ -48,6 +45,7 @@ function renderTimePort() {
     timePort.className = "portlet clearfix";
     timePort.innerHTML = '<div class="edit_controls"></div><h2 class="clearfix"><span class="moduleTitle">UK Time</span></h2><div class="collapsible" style="overflow: auto; aria-expanded="true" id="$fixedId"><div class="vtbegenerated"><p id="ukTime" style="text-align:center;font-family:\'Open Sans\',sans-serif;font-weight:bold;font-size:16px;color:rgb(0,0,0);overflow-wrap:break-word;margin:0;"></p></div></div>';
     document.getElementById("column0").appendChild(timePort);
+    document.getElementById("ukTime").innerText = calculateTime();
     setInterval(() => {
         document.getElementById("ukTime").innerText = calculateTime();
     }, 1000);
