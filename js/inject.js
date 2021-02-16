5777aeb8ccaf587bc59bce1121e6e5c7e13d8833
RyanXin <xyz@ryanxin.cn>
2021-02-17 00:40:08 +0800

    Added hide button.

--- js/inject.js
+++ js/inject.js
@@ -1,3 +1,16 @@
+let isCourseEditing = false;
 function editCourses() {
-    alert("editing")
+    isCourseEditing = !isCourseEditing;
+    if (isCourseEditing) {
+        let courses = document.getElementById("CurrentCourses").getElementsByTagName("ul")[0].getElementsByTagName("li");
+        for (let i = 0; i < courses.length; i++) {
+            courses[i].innerHTML += '<button class="hidebtn">X</button>';
+        }
+    }
+    else {
+        let courses = document.getElementById("CurrentCourses").getElementsByTagName("ul")[0].getElementsByTagName("li");
+        for (let i = 0; i < courses.length; i++) {
+            courses[i].innerHTML = courses[i].innerHTML.replace(/<button.*hidebtn.*>.*<\/button>/, "");
+        }
+    }
 }
