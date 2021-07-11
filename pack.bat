b10b83f748cc4a7004fa460e557e2f2b39704dda
RyanXin <xyz@ryanxin.cn>
2021-07-11 20:29:01 +0800

    chore: modify packing scripts

--- pack.bat
+++ pack.bat
@@ -2,9 +2,6 @@
 echo Did you update the version number and prepare to release?
 set /p ans=
 if /i not %ans% == y (pause && exit)
-
-cd "C:\Users\xyz11\OneDrive - University of Manchester\Programme\web"
+cd ..
 zip -r UoMBbEn/UoMBbEn.zip UoMBbEn/ -x **/.git/* **/preview/* **/.gitignore **/changelog.md **/ideas.txt **/pack.bat **/UoMBbEn.zip
-cd UoMBbEn
-
 pause
